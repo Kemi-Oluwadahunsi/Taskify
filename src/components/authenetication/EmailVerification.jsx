f4f39ce30a97a1fa5ba2610ad5029c5cb40011a5
TechMrs <kemilat50@gmail.com>
2024-11-02 17:57:04 +0800

    Update: Email verification process

--- src/components/authenetication/EmailVerification.jsx
+++ src/components/authenetication/EmailVerification.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 
 export default function EmailVerification() {
   const { emailToken } = useParams();
