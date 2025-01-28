ad84599c0848f76b20fbde71e11b8619cbbded1b
TechMrs <kemilat50@gmail.com>
2025-01-28 15:01:52 +0800

    Fix: .env issue

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  exclude: [".env"],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
